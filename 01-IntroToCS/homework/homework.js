b54af355ae18f47ad312154c1c4f56ba44444c4b
Julian Arbelaez <arbelaezjul2012@gmail.com>
2022-08-01 21:09:30 -0500

    primer cambio homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,14 +1,29 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  
+  let sum = 0;
+
+  for (let i = 0; i < num.length; i++) {
+     sum += +num[i] * 2 ** (num.length - 1 - i);
+  }
+  return sum;
+
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let num1 = num;
+    let binary = (num1 % 2).toString();
+    for (; num1 > 1; ) {
+        num1 = parseInt(num1 / 2);
+        binary =  (num1 % 2) + (binary);
+    }
+    console.log(binary);
 }
+
+
 
 
 module.exports = {
